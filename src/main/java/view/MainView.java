1cbde67d74696f635bf10d7b7e3deab8dd89e662
Otto Haertel <otto.haertel@siemens.com>
2021-02-07 14:02:21 +0100

    Kleine Änderunegn

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -19,7 +19,7 @@ public class MainView extends javax.swing.JFrame {
 
         private final static Logger LOGGER = Logger.getLogger(MainView.class.getName());
         private static final long serialVersionUID = 13L;
-        JButton liga1Btn;
+        javax.swing.JButton liga1Btn;
         JButton liga2Btn;
         JButton liga3Btn;
         JButton tableBtn;
